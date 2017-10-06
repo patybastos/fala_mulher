427b5ccb25969b8dd5ecede10d6c6fdd3509a040
Patricia Luz Bastos <patricia.bastos90@gmail.com>
2017-10-05 23:34:44 -0300

    add enumerize

--- Projetos/rails_girls/fala_mulher/app/models/post.rb
+++ Projetos/rails_girls/fala_mulher/app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
+  extend Enumerize
+
+  enumerize :kind, in: [:violence_physical, :violence_psychological, :violence_sexual, :violence_symbolic, :violence_patrimonial]
 
   validates :contact, :denouncement, :kind, presence: true
 
